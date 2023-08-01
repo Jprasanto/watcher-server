2cd6161b6865cda56fcb11c70d00aec0c750bd39
Jprasanto <joniprasanto@gmail.com>
2023-08-01 22:43:13 +0700

    add feat financial statement, midtrans still pending

--- routers/routerStock.js
+++ routers/routerStock.js
@@ -6,5 +6,7 @@ router.get('/', ControllerStock.ihsgData)
 
 router.get('/stocks/:symbol', ControllerStock.stockDataGraph)
 router.post('/wlist/:TickerId', ControllerStock.myWl)
+router.get('/fs/:symbol', ControllerStock.fsTicker)
+// router.post("/generate-midtrans-token", ControllerStock.midTrans);
 
 module.exports = router
